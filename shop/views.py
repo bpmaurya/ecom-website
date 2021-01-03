7ba5a7f6d2b9dec4daad381cde5275eedc5b3822
bpmaurya <realvedprakash@gmail.com>
2021-01-03 21:54:57 +0530

    change

--- shop/views.py
+++ shop/views.py
@@ -134,7 +134,7 @@ def checkout(request):
         }
         param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
         return render(request, 'shop/PayTm.html', {'param_dict': param_dict})
-
+    
     return render(request, 'shop/checkout.html')
 
 
